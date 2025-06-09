18867cadf5f235e9bf0dac4cf5470742e37daff5
Joshitha-Darshini <joshicorn2011@gmail.com>
2025-06-09 17:46:01 +0530

    Submit

---
+++ Module 1/Lesson 3/Activity2.py
@@ -0,0 +1,11 @@
+# Assigning Different Variables
+name= "Penguin"
+age= 15
+is_student = True
+Weight = 38.5
+# Printing Different Variables and thier Data Type
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
