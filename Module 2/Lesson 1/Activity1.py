1085e2a2f7526645386f43f5b483f54b52427b94
Joshitha-Darshini <joshicorn2011@gmail.com>
2025-06-16 18:54:54 +0530

    submit

---
+++ Module 2/Lesson 1/Activity1.py
@@ -0,0 +1,14 @@
+a= 10
+b= 10
+c= 0
+if a and b and c:
+    print("atleast one number has boolean value as true")
+else:
+    print("atleast one number has boolean value as false")
+a=10
+b=-10
+c=0
+if a > 0 or b > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
