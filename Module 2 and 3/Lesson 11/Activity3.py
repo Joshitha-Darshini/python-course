a359d64053dbe28532a252e0847a83c810a53641
Joshitha-Darshini <joshicorn2011@gmail.com>
2025-07-08 19:02:37 +0530

    submit

--- Module 2 and 3/Lesson 11/Activity3.py
+++ Module 2 and 3/Lesson 11/Activity3.py
@@ -1,5 +1,5 @@
 def factorial(x):
-'''this is arecursive function to find the factorial of an integer'''
+    '''this is a recursive function to find the factorial of an integer'''
     if x==0 or x==1:
         return 1
     else:
@@ -11,4 +11,4 @@ print("the factorial of 0:",factorial(0))
 print("the factorial of 1:",factorial(1))
 print("the factorial of 2:",factorial(4))
 print("the factorial of 5:",factorial(5))
-print("the factorial of 10:",ffactorial(10))
+print("the factorial of 10:",factorial(10))
