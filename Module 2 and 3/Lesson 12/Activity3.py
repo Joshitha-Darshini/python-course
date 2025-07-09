000baf858c66ef47e9f4800f516547ce1753f233
Joshitha-Darshini <joshicorn2011@gmail.com>
2025-07-09 18:32:13 +0530

    submit

---
+++ Module 2 and 3/Lesson 12/Activity3.py
@@ -0,0 +1,8 @@
+var = 10 #initialise
+while var > 0: #iterate loop
+    var =var -1
+    if var ==5: #condition 1
+        continue #continue statement
+        #display result
+    print('\ncurren variable value:', var)
+    print("\Good bye!")
