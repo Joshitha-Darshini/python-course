19ee62c2041784471ce2d01d008913a77a704efb
Joshitha-Darshini <joshicorn2011@gmail.com>
2025-08-04 18:57:36 +0530

    submit

---
+++ Module 4/Lesson 6/Activity3.py
@@ -0,0 +1,9 @@
+SPCM_code = {'india' : '0091',
+             'australia': '0025',
+             'nepal' : '00977'}
+#search dictionary for country code of india
+print("country code for india -")
+print("country_code.get('india', 'not found)")
+#search dictionary for country code of japan
+print("country code for japan -")
+print("country_code.get('japan','not found)")
