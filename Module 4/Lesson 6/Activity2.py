80da0cd851be76e1f5632d36b773e7849e3f8266
Joshitha-Darshini <joshicorn2011@gmail.com>
2025-08-04 18:50:40 +0530

    submit

---
+++ Module 4/Lesson 6/Activity2.py
@@ -0,0 +1,16 @@
+#initialize dictionary
+test_dict = {'codingal' : 2, 'is' : 2,
+'best' : 2, 'for' : 2,'coding' :1}
+#printing original dictionary
+print("the original dictionary :"+
+str(test_dict))
+#intitalize value
+K=2 
+#using loop 
+#selective key values in dictionary
+res = 0
+for key in test_dict:
+    if test_dict[key]== K :
+        res = res +1
+#printing result
+print("frequency of K is : " + str(res))     
